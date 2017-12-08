e505130340dd1d5ba52230d214d17457b2528a1b
dimanuz <malkovd@yandex.ru>
2017-12-08 22:35:48 -0100

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,160 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            colDeleteButtonEnabled: false,
+            rowDeleteButtonEnabled: false,
+            topStyle : {top: 60},
+            leftStyle : {left: 60},
+            data : null
+        };
+
+        this.totalCols = (props.totalCols || 4);
+        this.totalRows = (props.totalRows || 4);
+
+        this.selectedCol = -1;
+        this.selectedRow = -1;
+
+        //Жесткая привязка контекста
+        this.addColumn = this.addColumn.bind(this);
+        this.addRow = this.addRow.bind(this);
+        this.deleteColumn = this.deleteColumn.bind(this);
+        this.deleteRow = this.deleteRow.bind(this);
+        this.mouseOverCells = this.mouseOverCells.bind(this);
+        this.mouseOutCells = this.mouseOutCells.bind(this);
+        this.SetEnabledRowDeleteButton = this.SetEnabledRowDeleteButton.bind(this);
+        this.SetEnabledColDeleteButton = this.SetEnabledColDeleteButton.bind(this);
+        this.SetDisabledRowDeleteButton = this.SetDisabledRowDeleteButton.bind(this);
+        this.SetDisabledColDeleteButton = this.SetDisabledColDeleteButton.bind(this);
+    }
+
+    componentWillMount(){
+        this.setDataCells();
+    }
+
+    setDataCells() {
+        let arr = [];
+        for (let r = 0; r < this.totalRows; r++){
+            arr[r] = [];
+            for (let c = 0; c < this.totalCols; c++){
+                arr[r][c] = '';
+            }
+        }
+        this.setState({data: arr});
+    }
+
+    mouseOverCells(event) {
+        if (event.target.classList.contains('cells__td')) {
+            this.selectedCol = event.target.cellIndex;
+            this.selectedRow = event.target.parentElement.rowIndex;
+
+            if (this.totalCols > 1)
+                this.setState({colDeleteButtonEnabled: true});
+            if (this.totalRows > 1)
+                this.setState({rowDeleteButtonEnabled: true});
+
+            this.setState({topStyle: {top: event.nativeEvent.layerY-event.nativeEvent.offsetY}});
+            this.setState({leftStyle: {left: event.nativeEvent.layerX-event.nativeEvent.offsetX}});
+        }
+    }
+
+    mouseOutCells() {
+        this.SetDisabledRowDeleteButton();
+        this.SetDisabledColDeleteButton();
+    }
+
+    SetEnabledRowDeleteButton() {
+        if (this.totalRows > 1)
+            this.setState({rowDeleteButtonEnabled: true});
+    }
+
+    SetEnabledColDeleteButton() {
+        if (this.totalCols > 1)
+            this.setState({colDeleteButtonEnabled: true});
+    }
+
+    SetDisabledRowDeleteButton() {
+        this.setState({rowDeleteButtonEnabled: false});
+    }
+
+    SetDisabledColDeleteButton() {
+        this.setState({colDeleteButtonEnabled: false});
+    }
+
+    addColumn() {
+        let {data}  = this.state;
+        data.map(function (row) {return row.push('')});
+        this.setState({data: data});
+        this.totalCols++;
+    }
+
+    addRow() {
+        let {data}  = this.state;
+        let b = data[0].map(function (){ return '';});
+        data.push(b);
+        this.setState({data: data});
+        this.totalRows++;
+    }
+
+    deleteColumn() {
+        if (this.totalCols > 1) {
+            this.SetDisabledColDeleteButton();
+            let {data}  = this.state;
+            let c = this.selectedCol;
+            data.map(function (row) {return row.splice(c,1)});
+            this.setState({data: data});
+            this.totalCols--;
+        }
+    }
+
+    deleteRow() {
+        if (this.totalRows > 1) {
+            this.SetDisabledRowDeleteButton();
+            let {data}  = this.state;
+            data.splice(this.selectedRow,1);
+            this.setState({data: data});
+            this.totalRows--;
+        }
+    }
+
+    getClassNameDeleteColButton() {
+        const c = 'cells__buttons cells__delete_button cells__delete_column_button';
+        return this.state.colDeleteButtonEnabled? c+' cells__button-enabled' : c
+    }
+
+    getClassNameDeleteRowButton() {
+        const c = 'cells__buttons cells__delete_button cells__delete_row_button';
+        return this.state.rowDeleteButtonEnabled? c+' cells__button-enabled' : c
+    }
+
+    getData(){
+        return this.state.data.map(function (row, index) {
+            return <tr key={index}>{
+                row.map(function (td, index) {
+                return <td className='cells__td' key={index}>{td}</td>;
+            })
+        }</tr>;
+        });
+    }
+
+    render() {
+        return (
+            <div>
+            <table className = 'cells__table' onMouseOver = {this.mouseOverCells} onMouseOut = {this.mouseOutCells}>
+    <tbody>
+        {this.getData()}
+    </tbody>
+        </table>
+        <button className = 'cells__buttons cells__add_button cells__add_row_button' onClick = {this.addRow}><i className='fa fa-plus'></i></button>
+        <button className = 'cells__buttons cells__add_button cells__add_columm_button' onClick = {this.addColumn}><i className='fa fa-plus'></i></button>
+        <button style = {this.state.topStyle} className = {this.getClassNameDeleteRowButton()} onMouseOver = {this.SetEnabledRowDeleteButton} onMouseOut = {this.SetDisabledRowDeleteButton} onClick = {this.deleteRow}><i className='fa fa-minus'></i></button>
+        <button style = {this.state.leftStyle} className = {this.getClassNameDeleteColButton()} onMouseOver = {this.SetEnabledColDeleteButton} onMouseOut = {this.SetDisabledColDeleteButton} onClick = {this.deleteColumn}><i className='fa fa-minus'></i></button>
+        </div>
+    )
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './font-awesome/css/font-awesome.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let DOMCells = document.querySelectorAll(".cells");
+ReactDOM.render(
+    <App totalCols = "2" totalRows = "2" />,
+    DOMCells[0]
+);
+
+ReactDOM.render(
+    <App />,
+    DOMCells[1]
+);
+
 registerServiceWorker();
